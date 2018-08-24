74e99a93f0e73eab401d6f57c23956fd3be57771
omeyer <omeyer@student.wethinkcode.co.za>
2018-08-24 11:35:49 +0200

    playing around with how the server works

--- router/src/main/java/wethinkcode/fixme/router/server/Server.java
+++ router/src/main/java/wethinkcode/fixme/router/server/Server.java
@@ -4,27 +4,163 @@ import wethinkcode.fixme.router.routing.Router;
 import wethinkcode.fixme.router.server.Validation.MessageValidationHandler;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 import java.util.Set;
 
-
 public class Server {
+    // allows a single thread to examine I/O events on multiple channels
+    private Selector selector;
+    private InetSocketAddress listenAddress;
+    private final static int PORT = 19000;
+
+    public Server(String address, int port) throws IOException {
+        listenAddress = new InetSocketAddress(address, this.PORT);
+    }
+
+
+    private void startServer() throws IOException {
+        //allows the server to find multiple connections that are ready any I/0
+        this.selector = Selector.open();
+        //the serverChannel is only responsible for accepting new connections
+        ServerSocketChannel serverChannel = ServerSocketChannel.open();
+        //sets the serverChannel to a NIO
+        serverChannel.configureBlocking(false);
+        // bind server socket channel to port
+        serverChannel.socket().bind(this.listenAddress);
+        serverChannel.register(this.selector, SelectionKey.OP_ACCEPT);
+
+        System.out.println("Server started on port >> " + this.PORT);
+
+        while (true) {
+            //checks if there any available channels
+            int readyCount = this.selector.select();
+            if (readyCount == 0) {
+                continue;
+            }
+
+            // process selected keys...
+            Set<SelectionKey> readyKeys = selector.selectedKeys();
+            Iterator iterator = readyKeys.iterator();
+            while (iterator.hasNext()) {
+                SelectionKey key = (SelectionKey) iterator.next();
+                // Remove key from set so we don't process it twice
+                iterator.remove();
+                if (!key.isValid()) {
+                    continue;
+                }
+                if (key.isAcceptable()) { // Accept client connections
+                    this.accept(key);
+                } else if (key.isReadable()) { // Read from client
+                    this.read(key);
+                } else if (key.isWritable()) {
+                    // write data to client...
+                }
+            }
+        }
+    }
+
+    // accept client connection
+    private void accept(SelectionKey key) throws IOException {
+        ServerSocketChannel serverChannel = (ServerSocketChannel) key.channel();
+        SocketChannel channel = serverChannel.accept();
+        channel.configureBlocking(false);
+        Socket socket = channel.socket();
+        SocketAddress remoteAddr = socket.getRemoteSocketAddress();
+        System.out.println("Connected to: " + remoteAddr);
+
+        /**
+         * Register channel with selector for further IO (record it for read/write
+         * operations, here we have used read operation)
+         *
+         * change this later to allow writing as well
+         */
+        channel.register(this.selector, SelectionKey.OP_READ);
+    }
+
+    private void read(SelectionKey key) throws IOException {
+        SocketChannel channel = (SocketChannel) key.channel();
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        int numRead = -1;
+        numRead = channel.read(buffer);
+
+        if (numRead == -1) {
+            Socket socket = channel.socket();
+            SocketAddress remoteAddr = socket.getRemoteSocketAddress();
+            System.out.println("Connection closed by client: " + remoteAddr);
+            channel.close();
+            key.cancel();
+            return;
+        }
+
+        byte[] data = new byte[numRead];
+        System.arraycopy(buffer.array(), 0, data, 0, numRead);
+        System.out.println("Got: " + new String(data));
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*public class Server {
 
     private static final String POISON_PILL = "killall";
-    private static String IDNumber = "";
+    private static String IDNumber = ""; //this should not be a class attribute
     private static String message;
     private static Router router;
-    private static int    counter;
+    private static Map<String, SocketChannel> clientIdentifier = new HashMap<String, SocketChannel>();
 
     public Server(){
-        //Create a new router as the server is created on its port and update its Routing table
+        //TODO :Create a new router as the server is created on its port and update its Routing table
         router = new Router();
     }
 
@@ -32,23 +168,26 @@ public class Server {
 
         InetAddress ipAdress = InetAddress.getByName("localhost");
         int port = 19000;
-
         Selector selector = Selector.open();
         ServerSocketChannel channel = ServerSocketChannel.open();
-
         channel.configureBlocking(false);
         channel.socket().bind(new InetSocketAddress(ipAdress, port));
         channel.register(selector, SelectionKey.OP_ACCEPT);
+
         ByteBuffer buffer = ByteBuffer.allocate(256);
-
-
         while (true){
 
-            selector.select();
+            if (selector.select() == 0)
+                continue;
 
             Set<SelectionKey> selectionKeys = selector.selectedKeys();
             Iterator<SelectionKey> iter = selectionKeys.iterator();
-            while (iter.hasNext()){
+            /**
+             * I think the iterator should only be used find the appropriate key of the client currently
+             * sending the message
+             * (this suggestion might cause blocking im not sure)
+             */
+       /*     while (iter.hasNext()){
 
                 SelectionKey key = iter.next();
 
@@ -59,7 +198,7 @@ public class Server {
 
                 if (key.isReadable()){
                     //TODO:: Message Validation
-                   //answerWithEcho(buffer, key);
+                   answerWithEcho(buffer, key);
                 }
                 iter.remove();
             }
@@ -74,37 +213,46 @@ public class Server {
         client.write(ByteBuffer.wrap(IDNumber.getBytes()));
         client.configureBlocking(false);
         client.register(selector, SelectionKey.OP_READ);
-
+        clientIdentifier.put(IDNumber, client);
         //TODO :: Add connection to routing list NOte I don't know what we will use to identify a client so I am ...
         // TODO - Experimenting with the Selection key as the Identifier
         //this will be vital later when trying to send a message to the correct destination
         //{
-        router.addToRoutingTable(++counter, IDNumber, key);
+      //  router.addToRoutingTable(++counter, IDNumber, key);
         //} if it doesn't work this will be the problem
-    }
+    }*/
 
-    private static void answerWithEcho(ByteBuffer buffer, SelectionKey key)
+
+
+    /*private static void answerWithEcho(ByteBuffer buffer, SelectionKey key)
             throws IOException {
 
-        SocketChannel client = (SocketChannel) key.channel();
-        client.read(buffer);
+       // SocketChannel client = (SocketChannel) key.channel();
+        //client.read(buffer);
        // buffer.compact();
-        message = new String(buffer.array()).trim();
+        SocketChannel client = clientIdentifier.get("000001");
+        client.write(ByteBuffer.wrap(IDNumber.getBytes()));
+
+
+        //client.write(ByteBuffer.wrap(msg.getBytes()));
+        //client.read(buffer);
+        /*message = new String(buffer.array()).trim();
+        client.write(ByteBuffer.wrap(IDNumber.getBytes()));
         System.out.println(message);
         if (new String(buffer.array()).trim().equals(POISON_PILL)) {
             client.close();
             System.out.println("Not accepting client messages anymore");
         }
-        buffer.clear();
-    }
+        buffer.clear();*/
+  //  }
 
-    private static void  messageValidation(ByteBuffer buffer, SelectionKey key)
+  /*  private static void  messageValidation(ByteBuffer buffer, SelectionKey key)
     throws IOException{
 
         SocketChannel client = (SocketChannel) key.channel();
         client.read(buffer);
         message = new String(buffer.array()).trim();
 
-
     }
 }
+*/
