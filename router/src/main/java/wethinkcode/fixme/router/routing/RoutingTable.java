c1bbdbde27599df74d21bf64d4480d13efd35f97
omeyer <omeyer@student.wethinkcode.co.za>
2018-08-28 14:19:54 +0200

    most recent changes

--- router/src/main/java/wethinkcode/fixme/router/routing/RoutingTable.java
+++ router/src/main/java/wethinkcode/fixme/router/routing/RoutingTable.java
@@ -1,19 +1,21 @@
 package wethinkcode.fixme.router.routing;
 
+import lombok.Getter;
 import wethinkcode.fixme.router.server.Server;
 
 import java.nio.channels.SelectionKey;
 
+@Getter
 public class RoutingTable {
 
-    private String NetWorkDestination;
-    private int    id;
-    private SelectionKey channel;
+    private String netWorkDestination;
+    private String    id;
+    private SelectionKey key;
 
-    public RoutingTable(String netWorkDestination, int ID, SelectionKey channel){
-        this.id = ID;
-        this.NetWorkDestination = netWorkDestination;
-        this.channel = channel;
+    public RoutingTable(String netWorkDestination, String id, SelectionKey key){
+        this.id = id;
+        this.netWorkDestination = netWorkDestination;
+        this.key = key;
     }
 
 
