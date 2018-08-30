2f7be55df2bbd7ffac82fe63a1b3cf0df4341650
omeyer <omeyer@student.wethinkcode.co.za>
2018-08-30 10:20:48 +0200

    cleaned up code

--- router/src/main/java/wethinkcode/fixme/router/server/Validation/CheckSumValidator.java
+++ router/src/main/java/wethinkcode/fixme/router/server/Validation/CheckSumValidator.java
@@ -15,7 +15,7 @@ public class CheckSumValidator implements MessageValidationHandler {
     public boolean validateMessage(FixMessageValidator validMessage) {
 
        String message = validMessage.getMessage();
-       //System.out.println("this is msg: " + message);
+
        String[] tags = message.split("\\|");
        String checksum = tags[tags.length - 1].split("=")[1];
        message = "";
@@ -25,24 +25,16 @@ public class CheckSumValidator implements MessageValidationHandler {
         }
 
         String calculatedCheckSum = checkSumCalculator(message);
-        // We will go to the next calculation of the chain else we will send out an error message to the
-        // Client or Merchant telling them their message is invalid.
-       if (checksum.contentEquals(calculatedCheckSum)) {
-           System.out.println("checksum works");
-           nextChain.validateMessage(validMessage);
-
-           return true;
+        if (checksum.contentEquals(calculatedCheckSum)) {
+            nextChain.validateMessage(validMessage);
+              return true;
        }
        else {
-           //TODO Handle error messages on request
            return false;
        }
     }
 
     private String checkSumCalculator(String message){
-
-        //How to get check sum is basically the ASCII value of the message % 256
-        //basic C# representation is found at (http://gigi.nullneuron.net/gigilabs/calculating-the-checksum-of-a-fix-message/)
 
         String checkSum;
         String checkSumMessage = message.replace('|', '\u0001');
