87ccc6409055b9ccb0b7a2ad9bc006db98316300
omeyer <omeyer@student.wethinkcode.co.za>
2018-08-30 09:59:04 +0200

    streamline the RoutinTable class

--- router/src/main/java/wethinkcode/fixme/router/routing/RoutingTable.java
+++ router/src/main/java/wethinkcode/fixme/router/routing/RoutingTable.java
@@ -8,13 +8,11 @@ import java.nio.channels.SelectionKey;
 @Getter
 public class RoutingTable {
 
-    private String netWorkDestination;
     private String    id;
     private SelectionKey key;
 
-    public RoutingTable(String netWorkDestination, String id, SelectionKey key){
+    public RoutingTable(String id, SelectionKey key){
         this.id = id;
-        this.netWorkDestination = netWorkDestination;
         this.key = key;
     }
 
