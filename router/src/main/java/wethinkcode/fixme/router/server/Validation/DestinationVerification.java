a480cfc35d250130f7bc4d663bc22385da49fca6
omeyer <omeyer@student.wethinkcode.co.za>
2018-08-30 10:42:54 +0200

    added a new variable to the get the key for the selected destination

--- router/src/main/java/wethinkcode/fixme/router/server/Validation/DestinationVerification.java
+++ router/src/main/java/wethinkcode/fixme/router/server/Validation/DestinationVerification.java
@@ -1,13 +1,18 @@
 package wethinkcode.fixme.router.server.Validation;
 
+import lombok.Getter;
 import wethinkcode.fixme.router.routing.RoutingTable;
 
+import java.nio.channels.SelectionKey;
 import java.util.List;
 
+
+@Getter
 public class DestinationVerification implements MessageValidationHandler {
 
     private MessageValidationHandler nextChain;
     private List<RoutingTable> routingTables;
+    private SelectionKey key;
 
     public DestinationVerification(List<RoutingTable> routingTables) {
         this.routingTables = routingTables;
@@ -27,16 +32,17 @@ public class DestinationVerification implements MessageValidationHandler {
         String [] tags = validFixMessage.split("\\|");
         market = tags[5].split("=")[1];
 
-        for (RoutingTable item : routingTables){
+        for (RoutingTable item : this.routingTables){
             if (item.getId().equals(market)){
                 flag = true;
+                this.key = item.getKey();
                 break;
             }
         }
         if (!flag) {
             return false;
         } else {
-            nextChain.validateMessage(validMessage);
+            this.nextChain.validateMessage(validMessage);
             return true;
         }
     }
